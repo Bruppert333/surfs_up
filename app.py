1dceaca982dd810dd0aba5a659125eb67b4e2272
Bryce <bryceruppert@gmail.com>
2023-01-14 14:05:57 -0700

    Upload

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+# Set up the Database - access to our SQLite databse file
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+# Reflect the database
+Base.prepare(engine, reflect=True)
+
+# Create a variable for each of the classes
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link
+session = Session(engine)
+
+# Define our Flask App
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+
+# Define the welcome route
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
 
 
